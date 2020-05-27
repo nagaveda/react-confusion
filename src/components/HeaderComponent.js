3d185f6806dd8fae13e772b3d53291bf26e86679
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-05-27 17:01:28 +0530

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -30,7 +30,7 @@ class Header extends Component{
     render(){
         return(
             <React.Fragment>
-                <Navbar dark expand="md">
+                <Navbar fixed="top" dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
                         <NavbarBrand className="mr-auto" href="/">
