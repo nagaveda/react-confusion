04ee8f36a0ec270c2858a60144a7c28296b9796f
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-06-05 13:14:56 +0530

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import {Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem
 import {Link} from 'react-router-dom';
 import {Control, Errors, LocalForm} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -89,13 +90,15 @@ class CommentForm extends Component{
    function RenderDish({dish}){
         if(dish != null){
             return(
-                <Card>
-                    <CardImg width="100%" src = {dish.image} alt = {dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                
+                    <Card>
+                        <CardImg width="100%" src = {baseUrl +  dish.image} alt = {dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                
             );
         }
         else{
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText} from 'reactstrap';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const RenderCard = ({item, isLoading, errMess})=>{
     if(isLoading){
@@ -16,7 +17,7 @@ const RenderCard = ({item, isLoading, errMess})=>{
     else{
         return(
             <Card>
-                <CardImg src={item.image}/>
+                <CardImg src={baseUrl + item.image}/>
                 <CardBody>
                     <CardTitle>{item.name}</CardTitle>
                     {item.designation?<CardSubtitle>{item.designation}</CardSubtitle>:null}
@@ -33,10 +34,14 @@ function Home(props){
         <div className="Container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess = {props.dishesErrMess}/>
+                    <RenderCard item={props.dish} 
+                        isLoading={props.dishesLoading} 
+                        errMess = {props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion}/>
+                    <RenderCard item={props.promotion}
+                        isLoading={props.promosLoading} 
+                        errMess = {props.promosErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader}/>
